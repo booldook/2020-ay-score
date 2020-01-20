1e2eb2332cd3cf99d30b305b000a7ee8539303c4
booldook <booldook@gmail.com>
2020-01-20 15:02:09 +0900

    ajax 구현 시작

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-new WOW({offset: 200}).init();
+/*********** 컨트롤러 ***********/
+function init() {
+	$.ajax({
+		url: "http://webmir.co.kr/score/php/score_li.php",
+		success: getScore,
+		error: err
+	});
+}
+
+function getScore(res) {
+
+}
+
+function err(xhr) {
+	
+}
+
+
+
+
+
+
+/*********** 프로그램 시작 ***********/
+init();
