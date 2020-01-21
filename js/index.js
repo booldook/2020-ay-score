2f9c0629654b8211588929d3786d983140618df6
booldook <booldook@gmail.com>
2020-01-21 10:31:58 +0900

    index.html

--- js/index.js
+++ js/index.js
@@ -1,14 +1,68 @@
+/* 
+if(f.kor.value.trim() == "") {
+	alert("국어점수를 입력하세요.");
+	f.kor.focus();
+	return false;
+}
+if(Number(f.kor.value) > 100 || Number(f.kor.value) < 0) {
+	alert("국어점수를 올바르게 입력하세요. (0 ~ 100)");
+	f.kor.focus();
+	return false;
+}
+if(f.eng.value.trim() == "") {
+	alert("영어점수를 입력하세요.");
+	f.eng.focus();
+	return false;
+}
+if(Number(f.eng.value) > 100 || Number(f.eng.value) < 0) {
+	alert("영어점수를 올바르게 입력하세요. (0 ~ 100)");
+	f.eng.focus();
+	return false;
+}
+if(f.math.value.trim() == "") {
+	alert("수학점수를 입력하세요.");
+	f.math.focus();
+	return false;
+}
+if(Number(f.math.value) > 100 || Number(f.math.value) < 0) {
+	alert("수학점수를 올바르게 입력하세요. (0 ~ 100)");
+	f.math.focus();
+	return false;
+} 
+*/
+
+
 $("#btSave").click(function() {
+	var f = document.scoreForm;
+	var user = f.user.value.trim();
+	var stdname = f.stdname.value.trim();
+	var score = [];
+	var scoreName = [];
+	if(stdname == "") {
+		alert("학생이름을 입력하세요.");
+		f.stdname.focus();
+		return false;
+	}
+
+	for(var i=0; i<$(".score-input").length; i++) {
+		score[i] = Number($(".score-input").eq(i).val().trim());
+		scoreName[i] = $(".score-input").eq(i).attr("placeholder");
+		if(score[i] == "" || score[i] < 0 || score[i] > 100) {
+			alert(scoreName[i]+"점수를 올바르게 입력하세요 (0 ~ 100)");
+			$(".score-input").eq(i).focus();
+			return false;
+		}
+	}
 	$.ajax({
 		url: "http://webmir.co.kr/score/php/score_in.php",
 		type: "post",
 		dataType: "json",
 		data: { 
-			user: $("input[name='user']").val(), 
-			kor: $("input[name='kor']").val(), 
-			eng: $("input[name='eng']").val(), 
-			math: $("input[name='math']").val(), 
-			stdname: $("input[name='stdname']").val()
+			user: user, 
+			stdname: stdname,
+			kor: score[0], 
+			eng: score[1], 
+			math: score[2]
 		},
 		success: function(res){
 			if(res.code == 200) init();
@@ -20,6 +74,9 @@ $("#btSave").click(function() {
 
 
 function init() {
+	$("form[name='scoreForm']").find("input").each(function(i) {
+		if(i>0) $(this).val(""); // f.input.value = ""
+	});
 	$.ajax({
 		url: "http://webmir.co.kr/score/php/score_li.php",
 		type: "get",
